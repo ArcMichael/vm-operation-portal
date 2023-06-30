c44ac461912375bd3d2987378601f0d60e3b6b25
Michael Qiu <michaelqiu@MichaelMacBook-Pro-HOME.local>
2023-06-30 23:44:21 +0800

    feat(add next-auth add keycloak): sso

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,18 @@ import zhCN from "antd/locale/zh_CN";
 import "antd/dist/antd";
 import { ConfigProvider } from "antd";
 
-export default function App({ Component, pageProps }: AppProps) {
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
   return (
-    <ConfigProvider locale={zhCN}>
-      <Component {...pageProps} />
-    </ConfigProvider>
+    <SessionProvider session={session}>
+      <ConfigProvider locale={zhCN}>
+        <Component {...pageProps} />
+      </ConfigProvider>
+    </SessionProvider>
   );
 }
